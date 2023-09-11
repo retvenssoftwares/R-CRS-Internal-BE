434f71f233461a31f7ef44a9e12eb02797a99f67
preet-patil-4055 <preet.patil@retvensservices.com>
2023-09-11 18:31:48 +0530

    update

--- controllers/hotel/get_hotel_by_id.js
+++ controllers/hotel/get_hotel_by_id.js
@@ -7,7 +7,7 @@ module.exports.get_hotel_by_id = async (req, res) => {
 
   try {
     const data = await Hotel.findOne({ hotel_id: req.params.hotel_id })
-      .select('hotel_r_code hotel_name hotel_id hotel_logo hotel_cover_photo hotel_country hotel_images room_details hotel_address_line_1 hotel_address_line_2 hotel_ezee_code hotel_ezee_auth_code');
+      .select('hotel_r_code hotel_name hotel_id hotel_logo hotel_cover_photo hotel_country hotel_images room_details hotel_address_line_1 hotel_address_line_2 hotel_ezee_code hotel_ezee_auth_code hotel_description');
 
     if (!data) {
       return res.status(404).json({
@@ -17,8 +17,22 @@ module.exports.get_hotel_by_id = async (req, res) => {
 
     const response = await axios.post(`https://live.ipms247.com/booking/reservation_api/listing.php?request_type=RoomList&HotelCode=${data.hotel_ezee_code}&APIKey=${data.hotel_ezee_auth_code}&check_in_date=${check_in_date}&check_out_date=${check_out_date}`);
 
-    //const { min_ava_rooms } = response.data;
-    // console.log(response.data)
+    const extractedData = response.data.map(({ Roomtype_Name, roomtypeunkid, min_ava_rooms }) => ({
+      Roomtype_Name,
+      roomtypeunkid,
+      min_ava_rooms
+    }));
+
+    // Filter the room details based on the conditions
+    const filteredRoomDetails = data.room_details.map(roomDetail => {
+      const matchingRoom = extractedData.find(extractedRoom => extractedRoom.roomtypeunkid === roomDetail.room_type_id);
+
+      if (matchingRoom && parseInt(matchingRoom.min_ava_rooms) > 0) {
+        return roomDetail;
+      }
+
+      return null;
+    }).filter(Boolean);
 
     res.status(200).json({
       hotel_r_code: data.hotel_r_code,
@@ -28,10 +42,9 @@ module.exports.get_hotel_by_id = async (req, res) => {
       hotel_cover_photo: data.hotel_cover_photo,
       hotel_country: data.hotel_country,
       hotel_images: data.hotel_images,
-      room_details: data.room_details,
+      room_details: filteredRoomDetails, // Include filtered room details in the response
       hotel_address_line_1: data.hotel_address_line_1,
       hotel_address_line_2: data.hotel_address_line_2,
-      //min_ava_rooms: min_ava_rooms, // Include min_ava_rooms in the response
     });
   } catch (error) {
     // Handle errors, e.g., network errors or errors in the Axios request
