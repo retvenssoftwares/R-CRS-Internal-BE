0a2ae3cd6d56f875ccddec7ae140c656f945b116
Shubhamretvens <shubham.singh@retvensservices.com>
2023-09-12 20:14:45 +0530

    added top 8 hotel api

--- controllers/hotel/get_all_cities_hotels.js
+++ controllers/hotel/get_all_cities_hotels.js
@@ -12,17 +12,31 @@ module.exports.city = async (req, res) => {
 };
 
 
-module.exports.hotel_city = async(req,res) =>{
+module.exports.hotel_city = async (req, res) => {
   try {
+    const city = req.params.city;
 
-    const city  = req.params.city
-    const hotel = await Hotel.find({hotel_city : city});
+    let hotels;
 
-    return res.json(hotel);
+    if (city && city.trim() !== "") {
+      hotels = await Hotel.find({});
+     
+    } else {
+      // If no city is provided or it's an empty string, retrieve all hotels
+       hotels = await Hotel.find({ hotel_city: city });
+    }
+
+    if (hotels && hotels.length > 0) {
+      console.log(`Hotels in ${city || "all cities"} retrieved:`, hotels);
+      return res.json(hotels);
+    } else {
+      console.log(`No hotels found in ${city || "any city"}.`);
+      return res.status(404).json({ message: `No hotels found in ${city || "any city"}` });
+    }
   } catch (error) {
-    console.error("Error fetching cities:", error);
+    console.error("Error fetching hotels:", error);
     return res.status(500).json({ error: "Internal server error" });
   }
-  
-  
-}
+};
+
+
