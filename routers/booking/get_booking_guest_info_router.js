7e924c3342543405ede2bee6c238ff8ef3ea2965
Shubhamretvens <shubham.singh@retvensservices.com>
2023-09-15 13:34:05 +0530

    changes api url

--- routers/booking/get_booking_guest_info_router.js
+++ routers/booking/get_booking_guest_info_router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const  get_guest_details   = require("../../controllers/booking/get_booking_guest_details");
 
-router.get("/retvens/frmCustomerSearch.aspx",  get_guest_details.getdata);
-router.post("/post/retvens/frmCustomerSearch.aspx",  get_guest_details.postdata);
+router.get("/retvens/Customer",  get_guest_details.getdata);
+router.post("/post/retvens/Customer",  get_guest_details.postdata);
 
 module.exports = router;
