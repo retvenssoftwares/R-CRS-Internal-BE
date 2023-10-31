84f64ab2ea898716c3859bc9fa0d7dc95402fcf9
major99 <amandecembersharma@gmail.com>
2023-10-31 16:51:08 +0530

    gh action updated

--- server.js
+++ server.js
@@ -96,13 +96,22 @@ app.use('/api',fetchcoutries)
 app.use('/api',userlogin)
 
 //
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+      'PATCH',
+      'DELETE'
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+  
+  app.use(cors(corsOpts));
 
 mongoose
     .connect(process.env.DATABASE, {
